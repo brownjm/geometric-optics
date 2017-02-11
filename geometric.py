98f4107cfce97bf27bb71a20171c30650c62e6ba
Jeffrey M Brown <brown.jeffreym@gmail.com>
2017-02-11 12:03:52 +0100

    Reorganized code and added many comments. Particularly, I added a base class for all optical elements that handles the ABCD matrix. I also created pull out ray tracing code from view into it's own function

--- geometric.py
+++ geometric.py
@@ -1,150 +1,137 @@
 """Simple module for playing with geometric optics"""
 
-import numpy
-from matplotlib import pyplot as p
-import matplotlib.patches as patches
-from matplotlib import rcParams
 from cycler import cycler
+import numpy
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
-# higher resolution figures
-rcParams['figure.dpi'] = 200
-rcParams['axes.xmargin'] = 0.05
-rcParams['axes.ymargin'] = 0.05
+# increase the resolution of figures
+matplotlib.rcParams['figure.dpi'] = 200
+
+# add some space around optical system (zoom out)
+matplotlib.rcParams['axes.xmargin'] = 0.05
+matplotlib.rcParams['axes.ymargin'] = 0.05
+
 
 class Ray:
-    """Optical ray handles height and angle"""
+    """An optical ray is represented by a 2x1 vector containing its height
+and angle w.r.t. the optical axis"""
     def __init__(self, height, angle):
         self.height = height
         self.angle = angle
-        self.v = numpy.array((height, angle), ndmin=2).T
+        self.vector = numpy.array((height, angle), ndmin=2).T
 
     def __repr__(self):
         return "Ray({}, {})".format(self.height, self.angle)
 
 
-class RayTransferMatrix:
-    """ABCD matrix representation of an optical element"""
+
+class OpticalElement:
+    """An optical element is represented by a 2x2 matrix called an ABCD matrix"""
     def __init__(self, A, B, C, D):
         self.matrix = numpy.array(((A, B),
                                    (C, D)))
 
-    def dot(self, ray):
-        """Apply transfer matrix to ray"""
-        v = self.matrix.dot(ray.v)
-        return Ray(v[0][0], v[1][0])
+    def apply(self, ray):
+        """Apply the transfer matrix to the optical ray"""
+        height, angle = self.matrix.dot(ray.vector)
+        return Ray(height[0], angle[0])
         
 
-
-class FreeSpace:
+class FreeSpace(OpticalElement):
+    """Propagation in free space for a specified distance [mm]"""
     def __init__(self, distance):
         self.distance = distance
-        self.m = RayTransferMatrix(1, distance, 0, 1)
+        OpticalElement.__init__(self, 1, distance, 0, 1)
 
         
-class Lens:
+class Lens(OpticalElement):
+    """A thin lens with a given focus [mm]"""
     def __init__(self, focus):
         self.focus = focus
-        self.m = RayTransferMatrix(1, 0, -1.0 / focus, 1)
-
-
-class FlatMirror:
-    def __init__(self):
-        self.m = RayTransferMatrix(1, 0, 0, 1)
-
+        OpticalElement.__init__(self, 1, 0, -1.0 / focus, 1)
 
 
 
 class Scene:
+    """Optical rays and elements can be added to a scene and then displayed"""
     def __init__(self):
+        # rays and optical elements are stored separately as [(distance, item)]
         self.rays = []
-        self.elements = []
-        self.distance = 0
+        self.optical_elements = []
+
+        # keeps track of current distance when adding new optical elements
+        self.current_distance = 0
 
         
     def add(self, item):
+        """Add an item to the scene"""
         if isinstance(item, Ray):
-            self.rays.append((self.distance, item))
-        else:
+            self.rays.append((self.current_distance, item))
+        
+        elif isinstance(item, OpticalElement):
+            self.optical_elements.append((self.current_distance, item))
+            
             if isinstance(item, FreeSpace):
-                self.elements.append((self.distance, item))
-                self.distance += item.distance
-            else:
-                self.elements.append((self.distance, item))
+                # for free space propagation, the current distance is updated
+                self.current_distance += item.distance
+        
+        else:
+            raise TypeError("Cannot add object to scene: {}".format(type(item)))
 
             
     def view(self):
-        self.fig = p.figure()
-        self.ax = self.fig.add_subplot(111)
-        self.ax.set_prop_cycle(cycler('color', ['r', 'g', 'c', 'm']))
-
-        self.ax.axhline(ls='--', color='k')
+        """Display the scene"""
+        # create the figure and axes
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.set_prop_cycle(cycler('color', ['r', 'g', 'c', 'm']))
+        ax.axhline(ls='--', color='k')
+        ax.set_xlabel('Distance [mm]')
+        ax.set_ylabel('Radius [mm]')
+        ax.grid(True)
         
-        # plot elements
-        for z, element in self.elements:
-            self.display(z, element)
+        # display each optical element
+        for distance, element in self.optical_elements:
+            self._draw_element(ax, distance, element)
         
         # plot rays
         for distance, ray in self.rays:
-            path = [(0, ray.height)]
-            for z, element in self.elements:
-                if isinstance(element, FreeSpace):
-                    z += element.distance
-
-                    
-                ray = element.m.dot(ray)
-                path.append((z, ray.height))
-                # print(path)
+            path = self._trace(distance, ray)
+            plt.plot(*zip(*path), zorder=10)
 
 
-            p.plot(*zip(*path), zorder=10)
-
-
-        p.xlabel('Distance [mm]')
-        p.ylabel('Radius [mm]')
-        p.grid('on')
-        p.savefig('example.png')
-        p.show()
+        plt.savefig('example.png')
+        plt.show()
         
-
-        
-    def display(self, z, element, radius=20):
+  
+    def _draw_element(self, ax, distance, element, radius=20):
+        """Draw an optical element onto the current axes"""
         if isinstance(element, Lens):
-            self.ax.add_patch(patches.FancyArrowPatch((z, -radius),
-                                                      (z, radius),
-                                                      arrowstyle='<->',
-                                                      mutation_scale=40,
-                                                      lw=2,
-                                                      color='blue'))
-            
-            t = self.ax.text(z, radius+3, 'f = {} mm'.format(element.focus),
-                             horizontalalignment='center',
-                             verticalalignment='center')
+            # draw a lens as a double headed arrow <->
+            ax.add_patch(patches.FancyArrowPatch((distance, -radius), (distance, radius),
+                                                 arrowstyle='<->', mutation_scale=40,
+                                                 lw=2, color='blue'))
+
+            # add a text box above lens displaying the focal length
+            t = ax.text(distance, radius+3, 'f = {} mm'.format(element.focus),
+                        horizontalalignment='center', verticalalignment='center')
             t.set_bbox(dict(facecolor='white', alpha=1, edgecolor='black'))
 
+            
+    def _trace(self, z, ray):
+        """Trace a ray through each optical element"""
+        # save initial ray height
+        path = [(z, ray.height)]
+        
+        for distance, element in self.optical_elements:
+            # update distance for free space propagation
+            if isinstance(element, FreeSpace):
+                z += element.distance
 
-if __name__ == '__main__':
+            ray = element.apply(ray)
+            path.append((z, ray.height))
 
-    # scene will hold the optical rays and elements
-    scene = Scene()
-    
-    # rays are specified by their height [mm] and angle [radians] w.r.t. the optical axis
-    ray1 = Ray(height=0, angle=0.1)
-    ray2 = Ray(height=5, angle=0)
-    
-    # each item that we create must be added it to the scene
-    scene.add(ray1)
-    scene.add(ray2)
-    
-    # let's add a lens that is 100mm after the rays by using FreeSpace(100)
-    scene.add(FreeSpace(100))
-    scene.add(Lens(focus=50))
-    
-    # and add another lens of the same focal length 200mm after the first lens
-    scene.add(FreeSpace(200))
-    scene.add(Lens(focus=50))
-    
-    # finally let's allow the rays to propagate 100mm past the second lens
-    scene.add(FreeSpace(100))
-    
-    # let's view what we have made
-    scene.view()
+        return path
+
